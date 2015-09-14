d4dfa8b814c550c4398f7a0062d48d3f898e81ae
JJ Allaire <jj@rstudio.org>
2015-09-14 12:14:19 -0400

    implement cacheSolve for matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions to create and use a special data type that
+## caches matrix inversions. Uses R environments and lexcical
+## scoping rules to implements the cache.
 
-## Write a short comment describing this function
 
+## Create a matrix that can cache it's inverse 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    xInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) {
+    xInverse <<- inverse
+  }
+  getInverse <- function() {
+    xInverse
+  }
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Invert the matrix (directly return a cached version if available)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  mat <- x$get()
+  inverse <- solve(mat, ...)
+  x$setInverse(inverse)
+  inverse
 }
+
